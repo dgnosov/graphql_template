a0da0ccde6c3d011e04ec6371554dbe12ec61a3a
Daniel <ccsnosovdg@gmail.com>
2023-12-19 22:03:43 +0300

    feat: server basic query with graphql

--- server/index.js
+++ server/index.js
@@ -1,1 +1,42 @@
-console.log("server");
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+const cors = require("cors");
+const schema = require("./schema");
+
+const users = [{ id: 1, username: "Daniel", age: 33 }];
+
+const app = express();
+app.use(cors());
+
+const createUser = (input) => {
+  const id = Date.now();
+  return {
+    id,
+    ...input,
+  };
+};
+
+const root = {
+  getAllUsers: () => {
+    return users;
+  },
+  getUser: ({ id }) => {
+    return users.find((user) => user.id === id);
+  },
+  createUser: ({ input }) => {
+    const user = createUser(input);
+    users.push(user);
+    return user;
+  },
+};
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+    schema,
+    rootValue: root,
+  })
+);
+
+app.listen(5000, () => console.log("Server starts on port 5000"));
